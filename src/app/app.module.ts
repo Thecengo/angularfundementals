72945fa7e83c6afdcfdb04e6b89fc3c33ba61888
Thecengo <cengoyilmaz801@gmail.com>
2020-08-16 19:51:29 +0300

    angular services added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,24 @@ import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventThumbNailComponent } from './events/events-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.component';
+import { EventService } from './events/shared/event.service';
+import { ToastrService } from './common/toastr.service';
 
 @NgModule({
   declarations: [
     EventsAppComponent,
     EventsListComponent,
     EventThumbNailComponent,
-    NavBarComponent
+    NavBarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ 
+    EventService,
+    ToastrService
+  ],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
